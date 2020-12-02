e68eb135e8a69c6096ff193c3af2a311ce0b2e84
Yichen Xu <yichen.x@outlook.com>
2020-12-02 11:47:39 +0800

    Refactor file structure for deployment

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-import asyncio
-from coorml_runner.entry import Entry
-from coorml_runner.config import load_config
-import logging
-import argparse
-
+from coorml_runner.cli import main
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--config', type=str, default='config.yaml')
-    args = parser.parse_args()
-
-    logging.basicConfig(level=logging.INFO)
-    config = load_config(args.config)
-    entry = Entry(config)
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(entry.start())
+    main()
